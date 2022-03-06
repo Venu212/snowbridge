8ce635c3cdd3fee40ea35ae0836652340381db90
Venu Malireddy <venu.mv@gmail.com>
2022-03-06 11:20:31 -0800

    snowflake - read file added

--- db_functions/db_functions.py
+++ db_functions/db_functions.py
@@ -12,17 +12,19 @@ from snowflake.connector.pandas_tools import write_pandas
 
 
 import hashlib
-'''
-conn = snowflake.connector.connect(
-    user ='VENUGOPAL.MALIREDDY@SERVICENOW.COM',
-    authenticator='externalbrowser',
-    account='servicenow-edpdev',
-    warehouse='SNOW_DEV_WH',
-    database='SFD_EDW',
-    schema ='Sourcing'
-)
-sf_cur = conn.cursor()
-'''
+
+def conn_snowflake():
+    conn = snowflake.connector.connect(
+        user ='VENUGOPAL.MALIREDDY@SERVICENOW.COM',
+        authenticator='externalbrowser',
+        account='servicenow-edpdev',
+        warehouse='SNOW_DEV_WH',
+        database='SFD_EDW',
+        schema ='Sourcing'
+        )
+    sf_cur = conn.cursor()
+    return(sf_cur)
+
 import pyhdb
 def connect_hana():
     connection = pyhdb.connect(host="10.230.179.9",port=30115,user="Content_Migration",password="SJ0@1nWdspB$n")
@@ -34,7 +36,7 @@ from snowflake.sqlalchemy import URL
 from sqlalchemy import create_engine
 
 def connect_snowflake():
-    con = craete_engine(URL(
+    con = create_engine(URL(
         user ='VENUGOPAL.MALIREDDY@SERVICENOW.COM',
         authenticator='externalbrowser',
         account='servicenow-edpdev', 
@@ -42,22 +44,26 @@ def connect_snowflake():
         database=  'DEV_EDW_LS',         #'SFD_EDW',
         schema = 'FINANCE_AP_EM'        #'Sourcing'
         ))
-    sf_cur = con.cursor()
-    return(sf_cur)
+    #sf_cur = con.cursor()
+    return(con)
 
 
 
 def read_data_hana(hana_tbl, hana_cur):
    cur_hana_tbl = hana_cur.execute("select * from _SYS_BIC." +hana_tbl)
-   st.write(" HANA Tble Q= ","select * from _SYS_BIC." +hana_tbl)
+   #st.write(" HANA Tble Q= ","select * from _SYS_BIC." +hana_tbl)
    df_hana_tbl = pd.DataFrame(cur_hana_tbl.fetchall())
    df_hana_tbl.columns  = [i[0] for i in cur_hana_tbl.description]
    return(df_hana_tbl)
    #cur_prline_hana= cursor.execute('select * from "SURF_RT"."U_PURCHASE_REQUEST_LINE_ITEMS"')   
+
 def read_data_sf(sf_tbl, sf_cur):
+   #sf_cur.execute('use "DEV_EDW_LS"') 
    cur_sf_tbl = sf_cur.execute("select * from " +sf_tbl)
-   df_sf_tbl = pd.DataFrame(cur_sf_tbl.fetchall())
-   df_sf_tbl.columns  = [i[0] for i in cur_sf_tbl.description]
+   st.write ("SF SQL=","select * from " +sf_tbl)
+   df_sf_tbl = cur_sf_tbl.fetch_pandas_all()   
+   #df_sf_tbl = pd.read_sql(cur_sf_tbl, con =sf_cur )
+   #df_sf_tbl.columns  = [i[0] for i in cur_sf_tbl.description]
    return(df_sf_tbl)
 #def read_hana_data(hana_tbl, hana_cur):
     
